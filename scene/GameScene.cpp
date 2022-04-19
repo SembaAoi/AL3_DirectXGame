1f7f101b164f4bf3ea47886693e4fac0584c5b5a
SembaAoi <k021g1236@g.neec.ac.jp>
2022-04-19 14:45:16 +0900

    確認課題2_1の途中

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -8,8 +8,6 @@ GameScene::GameScene() {}
 
 GameScene::~GameScene() {
 
-	delete sprite_;
-
 	delete model_;
 }
 
@@ -23,9 +21,6 @@ void GameScene::Initialize() {
 	//ファイル名を指定してテクスチャを読み込む
 	textureHandle_ = TextureManager::Load("mario.jpg");
 
-	//スプライトの生成
-	sprite_ = Sprite::Create(textureHandle_, {100, 50});
-
 	// 3Dモデルの生成
 	model_ = Model::Create();
 
@@ -35,49 +30,25 @@ void GameScene::Initialize() {
 	//ビュープロジェクションの初期化
 	viewProjection_.Initialize();
 
-	//サウンドデータの読み込み
-	soundDataHandle_ = audio_->LoadWave("se_sad03.wav");
+	//X,Y,Z方向のスケーリングを設定
+	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
 
-	//音声再生
-	audio_->PlayWave(soundDataHandle_);
+	// X,Y,Z軸周りの回転角を設定
+	worldTransform_.rotation_ = {XM_PI / 4.0f, XM_PI / 4.0f, 0.0f};
 
-	//音声再生
-	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+	//X,Y,Z軸回りの平行移動を設定
+	worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
 
-	//音量調整
-	audio_->SetVolume(soundDataHandle_, 0.1f);
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+
+
 }
 
 void GameScene::Update() {
 
-	//スプライトの今の座標を取得
-	XMFLOAT2 position = sprite_->GetPosition();
-	//座標を{ 2,0 }移動
-	position.x += 2.0f;
-	position.y += 1.0f;
-	//移動した座標をスプライトに反映
-	sprite_->SetPosition(position);
 
-	//スペースキーを押した瞬間
-	if (input_->TriggerKey(DIK_SPACE)) {
-		//音声停止
-		audio_->StopWave(voiceHandle_);
-	}
-
-	//デバックテキストの表示
-	// debugText_->Print("Kaizokuou ni oreha naru.", 50, 50, 1.0f);
-
-	//書式指定付き表示
-	// debugText_->SetPos(50, 70);
-	// debugText_->Printf("year:%d", 2001);
-
-	//変数の値をインクリメント
-	value_++;
-	//値を含んだ文字列
-	std::string strDebug = std::string("Value:") + std::to_string(value_);
-	//デバッグテキストの表示
-	debugText_->Print(strDebug, 50, 50, 1.0f);
-}
+ }
 
 void GameScene::Draw() {
 
@@ -118,7 +89,6 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-	sprite_->Draw();
 
 	// デバッグテキストの描画
 	debugText_->DrawAll(commandList);
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -51,9 +51,6 @@ class GameScene {
 	//テクスチャハンドル
 	uint32_t textureHandle_ = 0;
 
-	//スプライト
-	Sprite* sprite_ = nullptr;
-
 	// 3Dモデル
 	Model* model_ = nullptr;
 
@@ -62,15 +59,6 @@ class GameScene {
 
 	//ビュープロジェクション
 	ViewProjection viewProjection_;
-
-	//サウンドデータハンドル
-	uint32_t soundDataHandle_ = 0;
-
-	//音声再生ハンドル
-	uint32_t voiceHandle_ = 0;
-
-	//値を表示したい変数
-	int32_t value_ = 0;
 
 	/// <summary>
 	/// ゲームシーン用
